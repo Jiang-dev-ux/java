59d5da5efc1e2a506a6bdecd77b7ebdd7cbb20e7
Jiang-dev-ux <1738447009@qq.com>
2021-06-01 23:38:02 +0800

    week9

---
+++ src/week9/driver.java
@@ -0,0 +1,12 @@
+package week9;
+
+import java.util.ArrayList;
+
+public class driver {
+	public static void main(String args[]) {
+		Commoditydao dao = new Commoditydao();
+		ArrayList<Commodity> list = dao.create();
+		dao.display(list);
+		
+	}
+}
